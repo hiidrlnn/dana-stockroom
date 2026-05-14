8c0baea454696f50dba7cce97f1f09b106a0227e
RESTU ADAM WIGUNA <restuadamwiguna@gmail.com>
2026-05-14 21:25:05 +0700

    menambahkan folder backend

---
+++ frontend/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7GUOFJTgWJw+uOnSrADY+egCkuyYq/V0mtjxRFDzFe4=\"\n}"
---
+++ frontend/.next/dev/static/chunks/src_app_(with-layout)_layout_tsx_06vcbe5._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/src_03-1qy2._.js","static/chunks/node_modules_0pxz6g.._.js"],
+    source: "dynamic"
+});
